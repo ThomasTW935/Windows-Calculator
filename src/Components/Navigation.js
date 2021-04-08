26304dfbe9a8dbab7191c866dbfb3196f94cd3fa
ThomasTW935 <thomastw935@gmail.com>
2021-04-09 00:38:34 +0800

    Fix design of navigation

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {  CATEGORIES } from '../App';
 
 function Navigation({category,setCategory, setType}) {
@@ -8,29 +8,36 @@ function Navigation({category,setCategory, setType}) {
         console.log(target)
         setCategory(target.value)
         setType(target.dataset.type)
+        setShowNav( prev=>{ return !prev } )
     }
-    
+
+    const [showNav,setShowNav] = useState(false)
+    const hiddenNav = (!showNav)? 'nav__list hidden' : 'nav__list' 
     return (
         <div className='nav'>
-            <div className='nav__burger'>
+            <div className='nav__burger' onClick={ ()=>{ setShowNav( (prev)=>{ return !prev } ) } }>
                 <div></div>
                 <div></div>
                 <div></div>
             </div>
-            <form className='nav__list'>
+            <form className={hiddenNav}>
                 {
                     Object.entries(CATEGORIES).map(([key,values],index)=>{
                         return <div key={index}>
                             <span className='category'>{key.toLowerCase()}</span>
-                                {values.map((value,i)=>{
-                                    return <label key={i}>
-                                        <input 
-                                            type='radio' 
-                                            name='category' 
-                                            value={value} 
-                                            checked={(value === category)}
-                                            data-type={key}
-                                            onChange={ (e)=> handleRadioChange(e) } />{value}</label>
+                            {values.map((value,i)=>{
+                                return <label key={i}>
+                                    <input 
+                                        type='radio' 
+                                        name='category' 
+                                        value={value} 
+                                        checked={(value === category)}
+                                        data-type={key}
+                                        onChange={ (e)=> handleRadioChange(e) } />
+                                    <span>#</span>
+                                    <span>{value}</span>
+                                        
+                                </label>
                             })}
                         </div>
                     })
