96c33061175789b5719f9ba63a1edcc45899c20d
ThomasTW935 <thomastw935@gmail.com>
2021-05-25 05:48:36 +0800

    Fix style of calculator buttons

--- src/Components/CalculatorTile.js
+++ src/Components/CalculatorTile.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function CalculatorTile({state}) {
     const {value,equation} = state
     return (
-        <div className='tile justify-right'>
+        <div className='tile tile-calculator justify-right'>
             <p className='equation'>{equation}</p>
             <p>{value}</p>
         </div>
